fa31c44eb83a017b60a3b9ac6549700b64933762
Milind220 <Milind220@users.noreply.github.com>
2026-03-29 04:01:37 +0000

    deploy: 735384f0bcf3cc3a82f8786fae84431039fdf68d

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-98fecffd.js',
+                path_to_root + 'searchindex-53dfbc64.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
